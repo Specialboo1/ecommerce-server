83fabbb3636cf78c8e198faaa5ae5034becd3022
Specialboo1 <84511434+Specialboo1@users.noreply.github.com>
2021-11-01 00:19:46 +0530

    url updated

--- index.js
+++ index.js
@@ -40,6 +40,6 @@ paytmParams['INDUSTRY_TYPE_ID'] = process.env.PAYTM_INDUSTRY_TYPE_ID,
 paytmParams['ORDER_ID'] = uuid(),
 paytmParams['CUST_ID'] = process.env.PAYTM_CUST_ID,
 paytmParams['TXN_AMOUNT'] = '100',
-paytmParams['CALLBACK_URL'] = 'http://localhost:8000/callback'
+paytmParams['CALLBACK_URL'] = 'https://boopalanecommerceserver.herokuapp.com/callback'
 paytmParams['EMAIL'] = 'boopalan1314@gmail.com'
 paytmParams['MOBILE_NO'] = '1234567890'
